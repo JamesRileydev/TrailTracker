fcf69a8ad9a5978a2536c8eed2b992681b9726f5
JamesRileydev <jamesriley.dev@gmail.com>
2020-02-22 17:33:07 -0500

    Added TrailsRepository

---
+++ TrailTracker.API/Data/TrailsRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TrailTracker.API.Data
+{
+    public interface ITrailsRepository
+    {
+
+    }
+
+
+    public class TrailsRepository : ITrailsRepository
+    {
+
+    }
+}
