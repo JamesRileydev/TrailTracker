909c67e2871d979c42674ba387c6752a097031e0
JamesRileydev <jamesriley.dev@gmail.com>
2020-03-04 06:21:33 -0500

    Added tests project

---
+++ TrailTracker.API.Tests/TrailsControllerUnitTests.cs
@@ -0,0 +1,15 @@
+using System;
+using Xunit;
+
+namespace TrailTracker.API.Tests
+{
+    [Trait("Category", "Unit")]
+    public class TrailsControllerUnitTests
+    {
+        [Fact]
+        public void Create()
+        {
+            Assert.True(true);
+        }
+    }
+}
